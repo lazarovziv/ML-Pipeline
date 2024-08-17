842b987cfa596b3d2361b4adf8b65a1de533d107
Ziv <zivlazarov@gmail.com>
2024-08-17 20:32:36 +0300

    airflow: added an additional task to example notifier dag

--- pipeline/airflow/dags/example_notifier.py
+++ pipeline/airflow/dags/example_notifier.py
@@ -12,8 +12,14 @@ with DAG(
     on_success_callback=ExampleNotifier('Success!'),
     on_failure_callback=ExampleNotifier('Failure!')
 ):
-    task = BashOperator(
-        task_id='example_task',
+    task0 = BashOperator(
+        task_id='example_task0',
         bash_command='exit 1',
         on_success_callback=ExampleNotifier('Task Succeeded!')
+    )
+
+    task1 = BashOperator(
+        task_id='example_task1',
+        bash_command='echo Hello $name!',
+        env={ 'name': 'Person' }
     )
