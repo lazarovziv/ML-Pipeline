16eff742aef313406fcc9c2bd90134bbd2ec7ce4
Ziv <zivlazarov@gmail.com>
2025-01-09 23:44:11 +0200

    api: modified optuna_report.py file to match api calls instead of direct calls to db

--- modules/db/optuna_report.py
+++ modules/db/optuna_report.py
@@ -1,70 +1,27 @@
 import os
 import asyncio
+import requests
 import psycopg2
 
-
-class DatabaseConnectionException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
+URL = os.environ['API_URL']
+PORT = os.environ['API_PORT']
+FULL_URL = f'{URL}:{PORT}' if PORT else URL
 
 class MissingQueryParameterException(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
 
-def init_db_connection():
-    if not os.path.exists('./.env'):
-        raise IOError('.env file doesn\'t exist. Can\'t initialize connection to the database!')
+'''
+to call from notebook,
 
-    # setting environment variables from .env file
-    with open('./.env', 'r') as f:
-        lines = f.read().splitlines()
-        for line in lines:
-            key, value = line.split('=')
-            os.environ[key] = value
+loop = asyncio.get_event_loop()
+loop.create_task(report_optuna_trial(study, trial))
 
-    try:
-        db_conn = psycopg2.connect(
-            user=os.environ['POSTGRES_USERNAME'],
-            password=os.environ['POSTGRES_PASSWORD'],
-            host=os.environ['POSTGRES_HOST'],
-            port=os.environ['POSTGRES_PORT'],
-            database=os.environ['POSTGRES_DB']
-        )
-
-        return db_conn
-    except Exception as e:
-        raise DatabaseConnectionException(str(e))
+'''
 
 
-def init_tables():
-    try:
-        connection = init_db_connection()
-
-        cursor = connection.cursor()
-        # read the queries from the .sql file
-        with open('./modules/db/sql/create_tables.sql', 'r') as f:
-            cursor.execute(f.read())
-
-        connection.commit()
-        cursor.close()
-        connection.close()
-    except DatabaseConnectionException as e:
-        print(f'{e} - Can\'t connect to the database! No tables will be created...')
-
-def execute_query(db_conn, cursor, query):
-    cursor.execute(query)
-    result = cursor.fetchall()[0]
-
-    db_conn.commit()
-    cursor.close()
-    db_conn.close()
-
-    return result
-
-
-def report_optuna_study(study, db_conn):
+async def report_optuna_study(study):
     study_distributions = study.get_trials(deepcopy=False)[0].distributions
 
     encoded_dim_min = study_distributions['encoded_dim'].low
@@ -96,92 +53,43 @@ def report_optuna_study(study, db_conn):
     relu_slope_min = study_distributions['relu_slope'].low
     relu_slope_max = study_distributions['relu_slope'].high
 
-    insert_study_query = f'''
-    INSERT INTO optuna_study(
-        encoded_dim_min,
-        encoded_dim_max,
-        initial_out_channels_min,
-        initial_out_channels_max,
-        learning_rate_min,
-        learning_rate_max,
-        weight_decay_min,
-        weight_decay_max,
-        momentum_min,
-        momentum_max,
-        dampening_min,
-        dampening_max,
-        scheduler_gamma_min,
-        scheduler_gamma_max,
-        kl_divergence_lambda_min,
-        kl_divergence_lambda_max,
-        epochs_min,
-        epochs_max,
-        batch_size_min,
-        batch_size_max,
-        beta1_min,
-        beta1_max,
-        beta2_min,
-        beta2_max,
-        optimizer_idx_min,
-        optimizer_idx_max,
-        relu_slope_min,
-        relu_slope_max
-    ) VALUES(
-        {encoded_dim_min},
-        {encoded_dim_max},
-        {initial_out_channels_min},
-        {initial_out_channels_max},
-        {learning_rate_min},
-        {learning_rate_max},
-        {weight_decay_min},
-        {weight_decay_max},
-        {momentum_min},
-        {momentum_max},
-        {dampening_min},
-        {dampening_max},
-        {scheduler_gamma_min},
-        {scheduler_gamma_max},
-        {kl_divergence_lambda_min},
-        {kl_divergence_lambda_max},
-        {epochs_min},
-        {epochs_max},
-        {batch_size_min},
-        {batch_size_max},
-        {beta1_min},
-        {beta1_max},
-        {beta2_min},
-        {beta2_max},
-        {optimizer_idx_min},
-        {optimizer_idx_max},
-        {relu_slope_min},
-        {relu_slope_max}
-    );
-    '''
+    json_data = {
+        'encoded_dim_min': encoded_dim_min,
+        'encoded_dim_max': encoded_dim_max,
+        'initial_out_channels_min': initial_out_channels_min,
+        'initial_out_channels_max': initial_out_channels_max,
+        'learning_rate_min': learning_rate_min,
+        'learning_rate_max': learning_rate_max,
+        'weight_decay_min': weight_decay_min,
+        'weight_decay_max': weight_decay_max,
+        'momentum_min': momentum_min,
+        'momentum_max': momentum_max,
+        'dampening_min': dampening_min,
+        'dampening_max': dampening_max,
+        'scheduler_gamma_min': scheduler_gamma_min,
+        'scheduler_gamma_max': scheduler_gamma_max,
+        'kl_divergence_lambda_min': kl_divergence_lambda_min,
+        'kl_divergence_lambda_max': kl_divergence_lambda_max,
+        'epochs_min': epochs_min,
+        'epochs_max': epochs_max,
+        'batch_size_min': batch_size_min,
+        'batch_size_max': batch_size_max,
+        'beta1_min': beta1_min,
+        'beta1_max': beta1_max,
+        'beta2_min': beta2_min,
+        'beta2_max': beta2_max,
+        'optimizer_idx_min': optimizer_idx_min,
+        'optimizer_idx_max': optimizer_idx_max,
+        'relu_slope_min': relu_slope_min,
+        'relu_slope_max': relu_slope_max,
+    }
 
-    cursor = db_conn.cursor()
-    cursor.execute(insert_study_query)
+    url = f'{FULL_URL}/optuna/study/new'
+    response = requests.post(url, json=json_data)
 
-    db_conn.commit()
-    cursor.close()
+    return response.status_code
 
-def report_optuna_trial(study, trial):
-    db_conn = init_db_connection()
-
-    cursor = db_conn.cursor()
-    # to get study_id we need to first insert the study into the db and then get the latest study record and extract the id
-    # because the study_id is auto generated incrementally
-    if trial.number == 0:
-        report_optuna_study(study, db_conn)
-
-    # get latest study id
-    cursor.execute('''
-        SELECT study_id
-        FROM optuna_study
-        ORDER BY study_id DESC
-        LIMIT 1
-    ''')
-    study_id = cursor.fetchall()[0][0]
-
+async def report_optuna_trial(study, trial):
     trial_id = trial.number
     trial_state = "'COMPLETED'" if trial.state == 1 else "'PRUNED'"
     trial_encoded_dim = trial.params['encoded_dim']
@@ -199,154 +107,42 @@ def report_optuna_trial(study, trial):
     trial_batch_size = trial.params['batch_size']
     trial_loss_function_id = trial.params['loss_idx']
     trial_relu_slope = trial.params['relu_slope']
-    
-    if trial.values:
-        # if a single objective optimization function
-        if len(trial.values) == 1:
-            trial_overall_loss_value = trial.values[0]
-            trial_kl_divergence_loss_value = -1.0
-            trial_loss_value = -1.0
-        # multi objective
-        else:
-            trial_kl_divergence_loss_value = trial.values[0] if trial.values is not None else -1.0
-            trial_loss_value = trial.values[1] if trial.values is not None else -1.0
-        if trial_kl_divergence_loss_value != -1.0 and trial_loss_value != -1.0:
-            trial_overall_loss_value = trial_loss_value + trial_kl_divergence_lambda * trial_kl_divergence_loss_value
-    # default values (NOT NULL)
-    else:
-        trial_overall_loss_value = -1.0
-        trial_kl_divergence_loss_value = -1.0
-        trial_loss_value = -1.0
 
-    insert_query = f'''
-    INSERT INTO optuna_trial(
-        study_id,
-        trial_id,
-        state,
-        encoded_dim,
-        initial_out_channels,
-        learning_rate,
-        weight_decay,
-        beta1,
-        beta2,
-        momentum,
-        dampening,
-        optimizer_idx,
-        scheduler_gamma,
-        kl_divergence_lambda,
-        epochs,
-        batch_size,
-        loss_function_id,
-        relu_slope,
-        overall_loss_value,
-        kl_divergence_loss_value,
-        loss_value
-    ) VALUES (
-        {study_id},
-        {trial_id},
-        {trial_state},
-        {trial_encoded_dim},
-        {trial_initial_out_channels},
-        {trial_learning_rate},
-        {trial_weight_decay},
-        {trial_beta1},
-        {trial_beta2},
-        {trial_momentum},
-        {trial_dampening},
-        {trial_optimizer_idx},
-        {trial_scheduler_gamma},
-        {trial_kl_divergence_lambda},
-        {trial_epochs},
-        {trial_batch_size},
-        {trial_loss_function_id},
-        {trial_relu_slope},
-        {trial_overall_loss_value},
-        {trial_kl_divergence_loss_value},
-        {trial_loss_value}
-    );
-    '''
+    json_data = {
+        'id': trial_id,
+        'state': trial_state,
+        'encoded_dim': trial_encoded_dim,
+        'initial_out_channels': trial_initial_out_channels,
+        'learning_rate': trial_learning_rate,
+        'weight_decay': trial_weight_decay,
+        'beta1': trial_beta1,
+        'beta2': trial_beta2,
+        'momentum': trial_momentum,
+        'dampening': trial_dampening,
+        'optimizer_idx': trial_optimizer_idx,
+        'scheduler_gamma': trial_scheduler_gamma,
+        'kl_divergence_lambda': trial_kl_divergence_lambda,
+        'epochs': trial_epochs,
+        'batch_size': trial_batch_size,
+        'loss_function_id': trial_loss_function_id,
+        'overall_loss_value': trial_overall_loss_value,
+        'kl_divergence_loss_value': trial_kl_divergence_loss_value,
+        'loss_value': trial_loss_value,
+        'relu_slope': trial_relu_slope
+    }
 
-    cursor.execute(insert_query)
+    url = f'{FULL_URL}/optuna/study/latest/trial/{trial_id}'
+    response = requests.post(url, json=json_data)
 
-    db_conn.commit()
-    cursor.close()
-    db_conn.close()
+    return response.status_code
 
-def report_study_best_loss_value(dataset_size):
-    db_conn = init_db_connection()
-    cursor = db_conn.cursor()
+async def get_best_hyperparameters(from_last_study=False):
+    url_path = 'latest/best_trial' if from_last_study else 'best_hyperparameters'
 
-    get_min_loss_value_in_last_study_query = '''
-    SELECT study_id, overall_loss_value, kl_divergence_loss_value, loss_value
-    FROM optuna_trial AS outr
-    WHERE overall_loss_value != -1
-    AND study_id = (SELECT MAX(inr.study_id)
-                    FROM optuna_study AS inr)
-    AND overall_loss_value = (SELECT MIN(inr.overall_loss_value)
-                                FROM optuna_trial AS inr
-                                WHERE inr.study_id = outr.study_id
-                                AND inr.overall_loss_value != -1)
-    '''
+    url = f'{FULL_URL}/optuna/study/{url_path}'
+    response = requests.get(url)
 
-    cursor.execute(get_min_loss_value_in_last_study_query)
-    study_id, best_overall_loss_value, best_kl_divergence_loss_value, best_loss_value = cursor.fetchall()[0]
-
-    update_best_loss_value_query = f'''
-        UPDATE optuna_study
-        SET best_overall_loss_value = {best_overall_loss_value},
-            best_loss_value = {best_loss_value},
-            best_kl_divergence_loss_value = {best_kl_divergence_loss_value},
-            dataset_size = {dataset_size}
-        WHERE study_id = {study_id};
-    '''
-    cursor.execute(update_best_loss_value_query)
-
-    db_conn.commit()
-    cursor.close()
-    db_conn.close()
-
-    return study_id
-
-def get_best_hyperparameters(from_last_study=False):
-    db_conn = init_db_connection()
-    cursor = db_conn.cursor()
-
-    if from_last_study:
-        condition = '''
-        study_id = (SELECT MAX(inr.study_id)
-                    FROM optuna_study AS inr)
-        AND overall_loss_value = (SELECT MIN(inr.overall_loss_value)
-                                    FROM optuna_trial AS inr
-                                    WHERE inr.study_id = outr.study_id
-                                    AND inr.overall_loss_value != -1)
-        '''
-    else:
-        condition = '''
-            overall_loss_value = (
-                SELECT MIN(inr.overall_loss_value)
-                FROM optuna_trial AS inr
-                WHERE inr.overall_loss_value != -1 AND
-                    inr.state != 'PRUNED' AND
-                    inr.loss_function_id = 0
-            )
-        '''
-
-    select_best_hyperparameters_query = f'''
-        SELECT encoded_dim, initial_out_channels, learning_rate, weight_decay, beta1, beta2, momentum,
-                dampening, optimizer_idx, scheduler_gamma, kl_divergence_lambda, epochs, batch_size, relu_slope
-        FROM optuna_trial AS outr
-        WHERE {condition};
-    '''
-    cursor.execute(select_best_hyperparameters_query)
-    (encoded_dim, initial_out_channels, learning_rate, weight_decay, beta1, beta2, momentum,
-     dampening, optimizer_idx, scheduler_gamma, kl_divergence_lambda, epochs, batch_size, relu_slope) = cursor.fetchall()[0]
-
-    db_conn.commit()
-    cursor.close()
-    db_conn.close()
-
-    return (encoded_dim, initial_out_channels, learning_rate, weight_decay, (beta1, beta2), momentum,
-            dampening, optimizer_idx, scheduler_gamma, kl_divergence_lambda, epochs, batch_size, relu_slope)
+    return response.json()
 
 def get_last_study_id():
     db_conn = init_db_connection()
